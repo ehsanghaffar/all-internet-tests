7a6636f32ee489e7bf2a74f5bcf5643dbcd68224
ehsanghaffarii <ghafari.5000@gmail.com>
2023-02-07 23:02:33 +0330

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ehsanghaffar/ultimate-internet-test
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/ehsanghaffar/ultimate-internet-test/tests"
+)
+
+func main() {
+
+	tests.CheckSpeed("https://ehsanghaffarii.ir")
+}
